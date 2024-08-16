d3ce263092bfd12dfc8f2f1cf7a249209b7e5938
Victor Lopez <vic.m.lopez114@gmail.com>
2024-08-15 23:40:53 -0400

    added functionality to links

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// nav scroll event
 window.addEventListener('scroll', function() {
   let navbar = document.getElementById('nav_bar');
 
@@ -14,14 +15,19 @@ let navOpenBtn = document.getElementById('hamburger-btn');
 let navModal = document.getElementById('nav_modal');
 let modalNavLinks = document.querySelectorAll('.modal_link');
 
+
+// opening mobile nav
 navOpenBtn.addEventListener('click', function() {
   navModal.style.visibility = 'visible';
 });
 
+// removing mobile nav
 navExitBtn.addEventListener('click', function() {
   navModal.style.visibility = 'hidden';
 });
 
+
+// allow mobile nav links to remove menu
 for (let i = 0; i < modalNavLinks.length; i++) {
   modalNavLinks[i].addEventListener('click', function() {
     navModal.style.visibility = 'hidden';
