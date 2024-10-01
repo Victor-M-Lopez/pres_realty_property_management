a257997b29bd5fc226970dcfd9d564bdcc9d9184
Victor Lopez <vic.m.lopez114@gmail.com>
2024-10-01 19:18:07 -0400

    added hamburger menu and its cool

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ let navBar = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
 	if (window.scrollY > 150) {
-		navBar.classList.add('solid');
 		navBar.classList.add('high-nav');
 	} else {
-		navBar.classList.remove('solid');
 		navBar.classList.remove('high-nav');
 	}
+});
+
+let navBtn = document.getElementById('hamburger_btn');
+const navModal = document.querySelector('.nav_modal');
+let exitBtn = document.querySelector('.exit_btn');
+
+navBtn.addEventListener('click', function () {
+	navModal.classList.toggle('nav_modal--visible');
+	navBar.style.height = '500px';
+	navBtn.style.visibility = 'hidden';
+	exitBtn.style.visibility = 'visible';
+});
+
+exitBtn.addEventListener('click', function () {
+	navModal.classList.toggle('nav_modal--visible');
+	navBar.style.height = '';
+	navBtn.style.visibility = 'visible';
+	exitBtn.style.visibility = 'hidden';
 });
