9297cb83a6681df67435e21e0e9dbfd752e5c516
Victor Lopez <vic.m.lopez114@gmail.com>
2024-09-30 20:24:55 -0400

    added so nav changes from fixed to sticky

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const navBar = document.querySelector('nav');
+let navBar = document.querySelector('nav');
+
 window.addEventListener('scroll', function () {
-	if (window.scrollY > 100) {
+	if (window.scrollY > 150) {
 		navBar.classList.add('solid');
+		navBar.classList.add('sticky-nav');
 	} else {
 		navBar.classList.remove('solid');
+		navBar.classList.remove('sticky-nav');
 	}
 });
